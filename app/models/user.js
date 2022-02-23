fa17b0b3dc3e2d02bf7f83a9d03b44db92db1baf
Mugammad <you@example.commugammadbreda1999@gmail.com>
2022-02-23 11:31:20 +0200

    user schema complete

--- app/models/user.js
+++ app/models/user.js
@@ -23,7 +23,11 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    cart: [product]
+    cart: {
+        type: Array,
+        required: false,
+        default: []
+    }
 })
 
 module.exports = mongoose.model('Product', productSchema)
